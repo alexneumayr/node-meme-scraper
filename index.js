02ba93fda0964556646d740d2948c86811b202a5
alexneumayr <alex.nmr@outlook.com>
2025-01-15 15:55:02 +0100

    Add function to access website

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { existsSync, mkdirSync } from 'node:fs';
 
+let websiteContent = '';
+
+// Create "memes" folder
 const folderName = './memes';
 try {
   if (!existsSync(folderName)) {
@@ -8,3 +11,27 @@ try {
 } catch (err) {
   console.error(err);
 }
+
+// Function to fetch the website
+const getWebsite = async () => {
+  try {
+    const response = await fetch(
+      'https://memegen-link-examples-upleveled.netlify.app',
+    );
+    if (response.ok) {
+      const textResponse = await response.text();
+      return textResponse;
+    } else {
+      throw new Error('Request failed!');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getWebsite()
+  .then((website) => {
+    websiteContent = website;
+    console.log(websiteContent);
+  })
+  .catch((error) => console.log(error));
